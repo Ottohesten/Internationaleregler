dcc6ddaa9baf4e93fa0962bb74b9b53d2b2cf358
Ottohesten <otto@skytop.dk>
2020-07-28 17:26:28 +0200

    added navbar

--- internationaleregler/tests.py
+++ internationaleregler/tests.py
@@ -4,11 +4,19 @@ from django.test import TestCase
 
 
 
-def factorial(n):
-    if n == 1:
-        return n
-    else:
-        return n*factorial(n-1)
+# def factorial(n):
+#     if n == 1:
+#         return n
+#     else:
+#         return n*factorial(n-1)
 
 
-print(factorial(5))
+# print(factorial(5))
+
+
+
+
+x1, x2 = 10, 20
+
+print(x1)
+print(x2)
