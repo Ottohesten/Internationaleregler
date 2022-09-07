e078aa41ea5bf6d45055f5fd58ad92b463572bc4
Otto <otto@skytop.dk>
2022-09-07 18:42:56 +0200

    Adding player profile

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
+from .models import Player
 
 # Register your models here.
+
+class PlayerInline(admin.StackedInline):
+    model = Player
+    can_delete = False
+    verbose_name_plural = 'Players'
+    
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (PlayerInline,)
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
---
+++ users/signals.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import Player
+
+
+@receiver(post_save, sender=User)
+def create_user_player(sender, instance, created, **kwargs):
+    if created:
+        Player.objects.create(user=instance)
+        
+@receiver(post_save, sender=User)
+def save_user_player(sender, instance, **kwargs):
+    instance.player.save()
