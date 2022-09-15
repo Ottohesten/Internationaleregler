f3d5128e1f280238a3e4efa811b5ae5aa90b5fcb
Otto <otto@skytop.dk>
2022-09-15 13:39:43 +0200

    fixed mistakes

--- forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from internationaleregler.models import RacismPoint
-
-
-class RacismPointForm(ModelForm):
-    class Meta:
-        model = RacismPoint
-        fields = "__all__"
--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from internationaleregler import views
-urlpatterns = [
-    path('', views.home, name="home"),
-    path('drengene/', views.drengene, name="drengene"),
-    path('racismelisten/', views.RacismPointListView.as_view(), name="racismelisten"),
-    path('racismelisten/create', views.RacismPointCreateView.as_view(), name="racismelisten-create"),
-]
