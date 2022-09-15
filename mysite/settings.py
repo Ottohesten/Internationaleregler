6e41856f98ae2fc7786e53cdab22a0172e7e573e
Otto Schmidt <otto@skytop.dk>
2022-09-15 22:46:48 +0200

    fixing exposed secret key

--- mysite/settings.py
+++ mysite/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = '1v3auye1f&@fb-ejr%%yx*)le%pov1qf5m*47t9%9vab51yc3)'
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
